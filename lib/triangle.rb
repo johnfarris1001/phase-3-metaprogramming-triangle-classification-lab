447afb86ff44ca536105e016807eb40441e90a3c
John <johnfarris217@gmail.com>
2023-05-07 13:40:35 -0500

    commit tree 14aa5058bf2cb44ac2bb5645d5c2f6f851337cbe

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,33 @@
 class Triangle
   # write code here
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+  
+  def kind
+    type = nil
+    if @a <= 0 || @b <= 0 || @c <= 0
+      raise TriangleError
+    elsif @a + @b <= @c || @a + @c <= @b || @b + @c <= @a
+      raise TriangleError
+    end
+
+    if @a == @b && @a == @c
+      type = :equilateral
+    elsif @a == @b || @a == @c || @b == @c
+      type = :isosceles
+    else
+      type = :scalene
+    end
+    type
+  end
+
+  class TriangleError < StandardError
+    def message
+      "This triangle is invalid!"
+    end
+  end
+
 end
